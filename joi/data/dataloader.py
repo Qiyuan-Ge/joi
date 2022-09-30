4bf0af9fc5eb7dead2141eeee43d2f2772711c6d
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2022-09-30 22:57:25 +0800

    Update dataloader.py

--- joi/data/dataloader.py
+++ joi/data/dataloader.py
@@ -7,13 +7,13 @@ def collate_fn_t5(batch):
     img_batch = []
     txt_batch = []
     for img, txt in batch:
-        img_batch.append(img)
+        img_batch.append(img.unsqueeze(0))
         txt_batch.append(txt)
-    img_batch = torch.tensor(img_batch)
-    input_ids, attn_mask = tokenize(txt_batch)
+    img_batch = torch.cat(img_batch, dim=0)
+    input_ids = tokenize(txt_batch)
     
-    return img_batch, (input_ids, attn_mask)
+    return img_batch, input_ids
     
          
-def Txt2ImgDataloader(dataset, batch_size, shuffle=True, collate_fn=collate_fn_t5):
-    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn)
+def Txt2ImgDataloader(dataset, batch_size, shuffle=True, num_workers=0, collate_fn=collate_fn_t5):
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, collate_fn=collate_fn)
