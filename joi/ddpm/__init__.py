31cd252b509eda4910bdd7b176a512c6c02b5493
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2022-10-01 21:34:28 +0800

    Update __init__.py

--- joi/ddpm/__init__.py
+++ joi/ddpm/__init__.py
@@ -1,8 +1,9 @@
 from .unet import Unet, SuperResUnet
 from .gaussian_diffusion import GaussianDiffusion
 from .train_util import Trainer
+from .pipeline import Painter
 
-__all__ = ['create_model', 'create_sr_model', 'create_gaussian_diffusion', 'create_model_and_diffusion', 'Trainer']
+__all__ = ['create_model', 'create_sr_model', 'create_gaussian_diffusion', 'create_model_and_diffusion', 'Trainer', 'Painter']
 
 def create_model(img_size=64, in_channels=3, num_res_blocks=2, out_channels=None, condition=None, text_model_name='t5-base', text_model_pretrained=True, num_classes=None, dropout=0):
     if img_size == 32:
