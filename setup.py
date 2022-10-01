190c33aba11fe3e1494383b63179e979a33b0df0
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2022-10-01 11:04:57 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name = "joi",
     version = 0.0,
-    author_email = "geqiyuan1105@gmail.com",
+    author = 'Qiyuan Ge'
+    author_email = 'geqiyuan1105@gmail.com',
     packages = find_packages(),
+    install_requires = [
+        'tqdm',
+        'numpy',
+        'einops',
+        'torch>=1.6',
+        'torchvision',
+        'transformers',
+        'sentencepiece',
+    ],
 )
