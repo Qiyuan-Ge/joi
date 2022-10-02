8591c5ab7349203215e00ced8c8dbcf19c5299a9
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2022-10-02 21:28:21 +0800

    Update dataloader.py

--- joi/data/dataloader.py
+++ joi/data/dataloader.py
@@ -35,6 +35,7 @@ class collate_fn:
         encoded_txt = self.encode_text(txt_batch)
 
         return img_batch, encoded_txt
-         
+
+    
 def Txt2ImgDataloader(dataset, batch_size, shuffle=True, text_model_name=DEFAULT_T5_NAME, num_workers=0, pin_memory=False):
     return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, collate_fn=collate_fn(text_model_name), pin_memory=pin_memory)
