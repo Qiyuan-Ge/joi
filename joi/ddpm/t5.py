6d401bb4fb9e9fcc3197ffeef7aa3cf3dff0e2d6
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2022-10-08 22:41:58 +0800

    Update t5.py

--- joi/ddpm/t5.py
+++ joi/ddpm/t5.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 import transformers
 from transformers import T5Tokenizer, T5EncoderModel, T5Config
 
@@ -20,25 +21,34 @@ def create_encoder(name=DEFAULT_T5_NAME, pretrained=True):
         return T5EncoderModel(T5Config.from_pretrained(name))
         
 
-def create_mask(txt, pad=PAD_id):
-    src_pad = txt != pad
-    src_pad_mask = src_pad.unsqueeze(1).expand(-1, txt.shape[1], -1)
+def create_mask(token_ids, pad=PAD_id):
+    src_pad = token_ids != pad
+    src_pad_mask = src_pad.unsqueeze(1).expand(-1, token_ids.shape[1], -1)
 
     return src_pad_mask
+
+
+def create_mask_and_weight(token_ids, pad=PAD_id):
+    src_pad = token_ids != pad
+    src_pad_mask = src_pad.unsqueeze(1).expand(-1, token_ids.shape[1], -1)
+    
+    mask = src_pad.float()
+    weight = F.softmax(mask.masked_fill(mask == 0, -1e12), dim=-1)
+
+    return src_pad_mask, weight
 
 
 def tokenize(texts, name=DEFAULT_T5_NAME, max_len=MAX_LENGTH):
     tokenizer = create_tokenizer(name)
     encoded = tokenizer.batch_encode_plus(texts, return_tensors="pt", padding='longest', max_length=max_len, truncation=True)
-    input_ids = encoded.input_ids
 
-    return input_ids
+    return encoded.input_ids
 
 
 def encode_text(texts, name=DEFAULT_T5_NAME, max_len=MAX_LENGTH, pad=PAD_id, eos=EOS_id, cuda=False):
     token_ids = tokenize(texts, name, max_len)
-    attn_mask = create_mask(token_ids, pad)
-    t5 = create_encoder(name=name)
+    attn_mask, weight = create_mask_and_weight(token_ids, pad)
+    t5 = create_encoder(name)
     if cuda:
         t5.cuda()
         token_ids = token_ids.cuda()
@@ -46,7 +56,6 @@ def encode_text(texts, name=DEFAULT_T5_NAME, max_len=MAX_LENGTH, pad=PAD_id, eos
     t5.eval()
     with torch.no_grad():
         output = t5(input_ids=token_ids, attention_mask=attn_mask)
-        encoded_text = output.last_hidden_state.detach()
-        encoded_text = encoded_text[token_ids==eos]
+        encoded_text = output.last_hidden_state.detach()    
         
-    return encoded_text
+    return (encoded_text * weight.unsqueeze(-1)).sum(-2)
