99619cbf21c21f6e83af88463b13c0e5408b89e4
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2022-10-09 16:59:32 +0800

    Update __init__.py

--- joi/ddpm/__init__.py
+++ joi/ddpm/__init__.py
@@ -6,14 +6,15 @@ from .gaussian_diffusion import GaussianDiffusion
 
 __all__ = ['create_model', 'create_sr_model', 'create_gaussian_diffusion', 'create_model_and_diffusion', 'Trainer', 'Painter']
 
-def create_model(img_size=64, in_channels=3, num_res_blocks=2, out_channels=None, condition=None, text_model_name='t5-base', num_classes=None, dropout=0):
+def create_model(img_size=64, in_dim=3, num_res_blocks=2, out_dim=None, condition=None, text_model_name='t5-base', num_classes=None, dropout=0):
     if img_size == 32:
-        return Unet(in_channels,
-                    model_channels=128,
-                    out_channels=out_channels,
+        return Unet(in_dim,
+                    d_model=128,
+                    out_dim=out_dim,
                     num_res_blocks=num_res_blocks,
-                    channel_mult=(1, 2, 2, 2),
+                    dim_mult=(1, 2, 2, 2),
                     layer_attention=(False, False, False, True),
+                    layer_cross_attention=(False, False, False, True),
                     dropout=dropout,
                     condition=condition,
                     text_model_name=text_model_name,
@@ -22,12 +23,13 @@ def create_model(img_size=64, in_channels=3, num_res_blocks=2, out_channels=None
                     num_heads_upsample=-1,
                     )
     elif img_size == 64:
-        return Unet(in_channels,
-                    model_channels=128,
-                    out_channels=out_channels,
+        return Unet(in_dim,
+                    d_model=128,
+                    out_dim=out_dim,
                     num_res_blocks=num_res_blocks,
-                    channel_mult=(1, 2, 3, 4),
+                    dim_mult=(1, 2, 3, 4),
                     layer_attention=(False, False, False, True),
+                    layer_cross_attention=(False, False, False, True),
                     dropout=dropout,
                     condition=condition,
                     text_model_name=text_model_name,
@@ -36,12 +38,13 @@ def create_model(img_size=64, in_channels=3, num_res_blocks=2, out_channels=None
                     num_heads_upsample=-1,
                     )
     elif img_size == 128:
-        return Unet(in_channels,
-                    model_channels=128,
-                    out_channels=out_channels,
+        return Unet(in_dim,
+                    d_model=128,
+                    out_dim=out_dim,
                     num_res_blocks=num_res_blocks,
-                    channel_mult=(1, 1, 2, 3, 4),
+                    dim_mult=(1, 1, 2, 3, 4),
                     layer_attention=(False, False, False, True, True),
+                    layer_cross_attention=(False, False, False, True, True),
                     dropout=dropout,
                     condition=condition,
                     text_model_name=text_model_name,
@@ -50,12 +53,13 @@ def create_model(img_size=64, in_channels=3, num_res_blocks=2, out_channels=None
                     num_heads_upsample=-1,
                     )
     elif img_size == 256:
-        return Unet(in_channels,
-                    model_channels=256,
-                    out_channels=out_channels,
+        return Unet(in_dim,
+                    d_model=256,
+                    out_dim=out_dim,
                     num_res_blocks=num_res_blocks,
-                    channel_mult=(1, 1, 2, 2, 4, 4),
+                    dim_mult=(1, 1, 2, 2, 4, 4),
                     layer_attention=(False, False, False, False, True, True),
+                    layer_cross_attention=(False, False, False, False, True, True),
                     dropout=dropout,
                     condition=condition,
                     text_model_name=text_model_name,
@@ -71,8 +75,8 @@ def create_gaussian_diffusion(model, timesteps=1000, beta_schedule='cosine', los
     return GaussianDiffusion(model, timesteps, beta_schedule, loss_type, p2_loss_weight_gamma, p2_loss_weight_k)
 
 
-def create_model_and_diffusion(img_size, in_channels=3, num_res_blocks=2, out_channels=None, timesteps=1000, beta_schedule='cosine', loss_type="l1", condition=None, text_model_name='t5-base', num_classes=None, dropout=0, p2_loss_weight_gamma=1.0, p2_loss_weight_k=1):
-    model = create_model(img_size, in_channels, num_res_blocks, out_channels, condition, text_model_name, num_classes, dropout)
+def create_model_and_diffusion(img_size, in_dim=3, num_res_blocks=2, out_dim=None, timesteps=1000, beta_schedule='cosine', loss_type="l1", condition=None, text_model_name='t5-base', num_classes=None, dropout=0, p2_loss_weight_gamma=1.0, p2_loss_weight_k=1):
+    model = create_model(img_size, in_dim, num_res_blocks, out_dim, condition, text_model_name, num_classes, dropout)
     diffusion = create_gaussian_diffusion(model, timesteps, beta_schedule, loss_type, p2_loss_weight_gamma, p2_loss_weight_k)
     
     return model, diffusion
