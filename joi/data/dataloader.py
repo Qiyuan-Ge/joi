cd32cd98319d1149fac0eb6584628e121cfcefef
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2022-10-12 16:38:31 +0800

    Update dataloader.py

--- joi/data/dataloader.py
+++ joi/data/dataloader.py
@@ -3,22 +3,6 @@ from functools import partial
 from torch.utils.data import DataLoader
 from joi.ddpm.t5 import tokenize, encode_text, DEFAULT_T5_NAME
 
-
-class collate_fn_old: # Deprecated
-    def __init__(self, text_model_name=DEFAULT_T5_NAME):
-        self.text_model_name = text_model_name
-    
-    def __call__(self, batch):
-        img_batch = []
-        txt_batch = []
-        for img, txt in batch:
-            img_batch.append(img.unsqueeze(0))
-            txt_batch.append(txt)
-        img_batch = torch.cat(img_batch, dim=0)
-        input_ids = tokenize(txt_batch, name=self.text_model_name)
-
-        return img_batch, input_ids
-
     
 class collate_fn:
     def __init__(self, text_model_name=DEFAULT_T5_NAME):
